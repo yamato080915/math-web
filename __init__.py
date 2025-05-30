8850dfd337f2849160813b721af8acc724b5db98
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-05-30 21:43:07 +0900

    problems

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask_login import login_required, current_user
+
+from models import User, MathProblems
+from app import db
 
 math = Blueprint(
 	"math", 
@@ -13,4 +17,27 @@ def latex():
 
 @math.route("/problems", methods=["GET", "POST"])
 def problems():
-	return render_template("math/problems.html")
+	return render_template("math/problems/index.html")
+
+@math.route("/problems/post", methods=["GET", "POST"])
+@login_required
+def post():
+	if request.method == "GET":
+		return render_template("math/problems/post.html")
+	else:
+		if not db.session.query(MathProblems).filter_by(user=int(current_user.get_id()), content=request.form["content"], category=request.form["category"], unit=request.form["unit"]).first():
+			problem = MathProblems(user=int(current_user.get_id()), content=request.form["content"], category=request.form["category"], unit=request.form["unit"])
+			db.session.add(problem)
+			db.session.commit()
+		p = db.session.query(MathProblems).filter_by(user=int(current_user.get_id()), content=request.form["content"], category=request.form["category"], unit=request.form["unit"]).first()
+		return redirect(url_for("math.problem", problem=p.id))
+
+@math.route("/problems/problem/<problem>")
+@login_required
+def problem(problem):
+	p = db.session.query(MathProblems).get(problem)
+	if p==None:
+		return "404"
+	else:
+		p = {"id":p.id, "user":p.user, "content":p.content, "category":p.category, "unit":p.unit, "created_at":p.created_at}
+		return render_template("math/problems/problem.html", data=p)
