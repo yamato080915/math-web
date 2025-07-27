d054b487eb6cf7c2dc45174d7b09d32e3a51752e
Yamat <145124662+yamato080915@users.noreply.github.com>
2025-07-27 19:08:37 +0000

    refactor: extract repeated MathJax update logic into a separate function

--- statics/js/event.js
+++ statics/js/event.js
@@ -1,9 +1,12 @@
+function mathjaxupdate(element, text) {
+    const formatted = text.replace(/\n/g, "<br>").replace("[終]", '<p style="text-align: right; padding-right: 10%;">(終)</p>');
+    element.innerHTML = `${formatted}`;
+    MathJax.typesetClear();
+    MathJax.typesetPromise([element]);
+}
 function latexevent(input, output) {
     const latexCode = input.value;
-    const formatted = latexCode.replace(/\n/g, "<br>");
-    output.innerHTML = `${formatted}`;
-    MathJax.typesetClear();
-    MathJax.typesetPromise([output]);
+    mathjaxupdate(output, latexCode);
 }
 
 function listener(input, output) {
